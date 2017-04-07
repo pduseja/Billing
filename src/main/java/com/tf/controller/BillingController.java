86e3a45d82ad4d8eab5b2c76df0ef2d86d1c96fc
pduseja <dusejapt@gmail.com>
2017-04-07 18:38:59 +0530

    Add files via upload

--- src/main/java/com/tf/controller/BillingController.java
+++ src/main/java/com/tf/controller/BillingController.java
@@ -2,6 +2,7 @@ package com.tf.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ public class BillingController {
 	
 	private Logger logger = Logger.getLogger(BillingController.class);
 	
+	@CrossOrigin(origins="*")
 	@RequestMapping(value="/doBilling",method=RequestMethod.POST)
 	public Boolean doBilling(@RequestBody Billing billing) {
 		logger.info("Billing Service Started....");
